73c1d396fab980827ce3f1f36a0211c6aa4d4044
Aliacf21 <51883210+Aliacf21@users.noreply.github.com>
2020-10-28 15:23:58 -0400

    working card

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { Card, Button, Modal, ButtonGroup, ToggleButton } from "react-bootstrap";
+import { Card, Button, Modal, ButtonGroup, ToggleButtonGroup, ToggleButton } from "react-bootstrap";
 
 
 
@@ -30,6 +30,7 @@ class About extends Component {
     //Using || backup assignment
     todoList: loaded_todoList || default_todoList,
     newTodoContent: ""
+
   };
 
    toggleDone = (evt, i) => {
@@ -88,37 +89,49 @@ class About extends Component {
     */
  
 
-  const MyModal = ({title}) => { 
+
+  let customerChoiceForQuantity = "1";
+  let customerChoiceForGlazing = "None";
+
+
+
+  const MyModal = ({title, price}) => { 
     const [show, setShow] = React.useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     const customerChoice = []
+    var test = { "name": 0};
 
     return (
-      <>
+      <div>
         <Button onClick={handleShow} variant="light" size="lg" style={{backgroundColor: "white", color:"black", padding: 0}}><u>{title}</u></Button> 
          <Modal show={show} onHide={handleClose} backdrop="static"  animation={true}  size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
            <Modal.Header closeButton style={{backgroundColor: "orange", color: "white", fontSize: "large"}}>
             <Modal.Title>{title}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-           <div class="row">    
+           <div class="row"> 
+
             <div class="col-lg-7">
               <Card.Img style={{ height: '28rem' }} variant="top" src="https://www.cookingclassy.com/wp-content/uploads/2012/12/45+minute+cinnamon+rolls9.jpg" />
             </div>
 
             <div class="col-lg-5">
-              <ButtonType title={"Glazing"} button1 = {"None"} button2 = {"Vanilla-Milk"} button3 = {"Sugar-Milk"} button4={"Double"}/>
-              <ButtonType title={"Quantity"} button1 = {"1"} button2 = {"2"} button3 = {"6"} button4={"12"}/>
+              <ButtonType title={"Glazing"} num = {test} button1 = {"None"} button2 = {"Vanilla-Milk"} button3 = {"Sugar-Milk"} button4={"Double"} price={price}/>
+              <ButtonType title={"Quantity"} button1 = {"1"} button2 = {"2"} button3 = {"6"} button4={"12"} price={price}/>
+
+              <ButtonType1 title={"Order"} button1 = {"1"} button2 = {"2"} button3 = {"6"} button4={"12"} price={price}/>
             </div>
-          </div>
+      </div>
+
+
         </Modal.Body>
         <Modal.Footer>
           <Button variant="link" style={{color: "black"}} onClick={handleClose}> Continue Browsing </Button>
           <Button variant="outline-light" style={{color: "white", backgroundColor: "orange"}} onClick={handleClose}> Add to Cart </Button>
         </Modal.Footer>
       </Modal>
-    </>
+    </div>
     );
   }
 
@@ -130,9 +143,9 @@ class About extends Component {
         <Card.Img style={{ height: '28rem' }} variant="top" src="https://www.cookingclassy.com/wp-content/uploads/2012/12/45+minute+cinnamon+rolls9.jpg" />
           <div class="card-body">
               <div class="row align-items-center">  
-                <div class="col-lg-10 align-items-center"><MyModal title ={"hello"}/></div>
+                <div class="col-lg-10 align-items-center"><MyModal title ={title} price={price}/></div>
                 <div class="col-lg-2 align-items-center">  
-                  <Card.Title>{price}</Card.Title>
+                  <Card.Title>${price}</Card.Title>
                 </div>
        
               </div>
@@ -143,47 +156,111 @@ class About extends Component {
       </div>);
   }
 
+  const ButtonType = ({title, num, button1, button2, button3, button4, price}) => { 
 
-  const ButtonType = ({title, button1, button2, button3, button4}) => { 
 
-    let customerChoice = []
+
+       const updateValue = (someValue) => {
+        if (title == "Quantity") {
+        customerChoiceForQuantity = someValue;
+        console.log("Quantity: " + customerChoiceForQuantity)
+        document.getElementById("currentQuatity").innerHTML = customerChoiceForQuantity;
+
+      }
+      else {
+        customerChoiceForGlazing = someValue;
+        console.log("Glazing: " + customerChoiceForGlazing);
+        document.getElementById("currentGlaze").innerHTML = customerChoiceForGlazing;
+      }
+      document.getElementById("currentTotal").innerHTML = "$" + price * customerChoiceForQuantity;
+    }
+
+
+   
     return (
-
     <div class="row">
       <p>{title}</p>
         <div class="row align-items-center"></div>
+        <div>
 
           <div class="row align-items-center">
             <div class="col-lg-6">
-              <Button as="input" onClick={() => this.addItem("PLEASE????")} size="sm" variant="light" style={{width: "9rem", backgroundColor:"orange", color:"white"}} value={button1} />
+              <Button onClick={() => updateValue(button1)} size="sm" variant="light" style={{width: "9rem", backgroundColor:"orange", color:"white"}} value={button1}>{button1}</Button>
+
             </div>
 
             <div class="col-lg-6">
-              <Button as="input" onClick={() => customerChoice.push("Vanilla Milk")} size="sm" variant="light" style={{width: "9rem", backgroundColor:"orange", color:"white"}} value={button2} />
+              <Button onClick={() => updateValue(button2)}  size="sm" variant="light" style={{width: "9rem", backgroundColor:"orange", color:"white"}} value={button2}>{button2}</Button>
             </div>
           </div>
          
           <div class="row my-2 align-items-center">
               <div class="col-lg-6">
-                <Button as="input" onClick={() => customerChoice.push("Sugar Milk")} size="sm" variant="light" style={{width: "9rem", backgroundColor:"orange", color:"white"}} value={button3} />
+                <Button onClick={() => updateValue(button3)}  size="sm" variant="light" style={{width: "9rem", backgroundColor:"orange", color:"white"}} value={button3}>{button3}</Button>
               </div>
 
             <div class="col-lg-6">
-                <Button as="input" onClick={() => customerChoice.push("Double")} size="sm" variant="light" style={{width: "9rem", backgroundColor:"orange", color:"white"}} value={button4} />
+                <Button onClick={() => updateValue(button4)} size="sm" variant="light" style={{width: "9rem", backgroundColor:"orange", color:"white"}} value={button4}>{button4}</Button>
             </div>
+          </div>
+
 
           </div>
-    </div>
+          </div>
 
           );
       }
 
 
 
+  const ButtonType1 = ({title, num, button1, button2, button3, button4, price}) => { 
+   
+    return (
+    <div class="row">
+      <p>{title}</p>
+
+          <div class="row align-items-center">
+            <div class="col-lg-6">
+              <Button size="sm" variant="light" style={{width: "9rem", backgroundColor:"white", color:"black", textAlign: "left"}}>Glazing</Button>
+
+            </div>
+
+            <div class="col-lg-6">
+              <Button   size="sm" id="currentGlaze" variant="light" style={{width: "9rem", backgroundColor:"white", color:"black", textAlign: "right"}} value={customerChoiceForGlazing}>{customerChoiceForGlazing}</Button>
+            </div>
+          </div>
+         
+          <div class="row my-2 align-items-center">
+              <div class="col-lg-6">
+                <Button  size="sm" variant="light" style={{width: "9rem", backgroundColor:"white", color:"black", textAlign: "left"}}>Quantity</Button>
+              </div>
+
+            <div class="col-lg-6">
+                <Button  size="sm" id="currentQuatity" variant="light" style={{width: "9rem", backgroundColor:"white", color:"black", textAlign: "right"}} value={customerChoiceForQuantity}>{customerChoiceForQuantity}</Button>
+            </div>
+          </div>
+
+
+           <div class="row my-2 align-items-center">
+              <div class="col-lg-6">
+                <Button  size="sm" variant="light" style={{width: "9rem", backgroundColor:"white", color:"black", textAlign: "left"}}>Total</Button>
+              </div>
+
+            <div class="col-lg-6">
+                <Button  size="sm" id="currentTotal" variant="light" style={{width: "9rem", backgroundColor:"white", color:"black", textAlign: "right"}} value={price}>${price}</Button>
+            </div>
+          </div>
+
+          </div>
+
+          );
+      }
+
+
 
     row1.push(
       <div class="col-lg-4">
-        <MyCard title={"Original"} />
+        <MyCard title={"Original"} price={1}/>
       </div>
     )
 
