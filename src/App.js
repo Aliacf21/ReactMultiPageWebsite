7bcebe2e71e0cd63bf1e73432dd0d2b1f9d6aaa4
Aliacf21 <51883210+Aliacf21@users.noreply.github.com>
2020-10-29 16:30:06 -0400

    working count

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, {Component} from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Navigation, Footer, Home, About, Contact } from "./components";
 
-const default_count = 1
+const default_count = 0
 
 function load(){
   let count = localStorage.getItem('count')
   //Using && to condition on str_todos else null
+  if (count < 0) {
+    localStorage.setItem('count',JSON.stringify(0))
+  }
   return (count && JSON.parse(count)) 
 }
 
@@ -14,33 +17,37 @@ function store(count){
   localStorage.setItem('count',JSON.stringify(count))
 }
 
+let load_count = load()
 
 class App extends Component {
 
   state = {
-    count: default_count || load()
+    count: load_count || default_count
   };
 
- componentDidMount() {
-    let loaded_count = load()
-     this.setState(
-    //Using || backup assignment
-    {todoList: loaded_count || default_count
-    }
-  );
-    }
+
 
 incrementCount = () => {
+  load_count = load_count + 1;
+  store(load_count)
+  console.log("There" + this.state.count)
   this.setState((state) => {
-    return {count: (this.count + 1)}
+    return {count: (this.state.count + 1)}
   });
 
 }
 
 decrementCount = () => {
-  let defualt_count = load()
+  load_count = load_count-1;
+  store(load_count)
+  if (this.state.count == 0) {return }
+  if (this.state.count < 0) { 
+    this.setState((state) => {
+    return {count: 0}
+  });}
+
   this.setState((state) => {
-    return {count: this.count - 1}
+    return {count: this.state.count - 1}
   });
 }
 
