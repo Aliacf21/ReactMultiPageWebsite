f3f8ff9f2ad1173d5b28781b69d1e39cfba6b1a1
Aliacf21 <51883210+Aliacf21@users.noreply.github.com>
2020-10-29 08:12:55 -0400

    wokring urls take 2

---
+++ docs/precache-manifest.22f7c30a7af1dc95b60a65649ff55ced.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c058bf7ee348248d30593346d5219a",
+    "url": "/ReactMultiPageWebsite/index.html"
+  },
+  {
+    "revision": "48ce504482eecfbffea9",
+    "url": "/ReactMultiPageWebsite/static/css/2.3e575b9f.chunk.css"
+  },
+  {
+    "revision": "2c42ca782ea2f35b2e3f",
+    "url": "/ReactMultiPageWebsite/static/css/main.d63071f5.chunk.css"
+  },
+  {
+    "revision": "48ce504482eecfbffea9",
+    "url": "/ReactMultiPageWebsite/static/js/2.b4e9b349.chunk.js"
+  },
+  {
+    "revision": "2e54b551fdc3988dd651d638460fe789",
+    "url": "/ReactMultiPageWebsite/static/js/2.b4e9b349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c42ca782ea2f35b2e3f",
+    "url": "/ReactMultiPageWebsite/static/js/main.20813786.chunk.js"
+  },
+  {
+    "revision": "1e5fd03b5a840e1dccf5",
+    "url": "/ReactMultiPageWebsite/static/js/runtime-main.696fb67e.js"
+  }
+]);
